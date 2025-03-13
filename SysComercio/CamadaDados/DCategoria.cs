8532df333f74fb3702556817cb1759d4e9ea160b
rafaelgrohl222 <rafaelgrohl222@gmail.com>
2025-03-12 22:08:03 -0400

    Sistema Comércio: framework C#, SQL Server, Procedimento em SQL Server

--- SysComercio/CamadaDados/DCategoria.cs
+++ SysComercio/CamadaDados/DCategoria.cs
@@ -8,7 +8,7 @@ using System.Data.SqlClient;
 
 namespace CamadaDados
 {
-    class DCategoria
+    public class DCategoria
     {
         private int _Id_categoria;
         private string _Nome;
@@ -125,7 +125,7 @@ namespace CamadaDados
                 ParDescricao.Value = Categoria.Descricao;
                 SqlCmd.Parameters.Add(ParDescricao);
 
-                //Executar o comando = se inserido informar: OK se não "Registro não foi Inserido!"
+                //Executar o comando = se edição informar: OK se não "Registro não foi Inserido!"
                 resp = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "A edição não foi realizada!";
 
             }
@@ -141,12 +141,92 @@ namespace CamadaDados
             }
         }
         //Método Excluir
-        public string Excluir(DCategoria Categoria) { }
-        //Método Mostrar
-        public DataTable Mostrar(DCategoria Categoria) { }
-        //Método Buscar Nome
-        public string BuscarNome(DCategoria Categoria) { }
+        public string Excluir(DCategoria Categoria) 
+        {
+            string resp = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexao.Cn;
+                SqlCon.Open();//Aberta a conexão
 
-        //Aula 29, 0min03seg
+                //Chamando o procedimento / procedure
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;//Receber a conexão
+                SqlCmd.CommandText = "spdeletar_categoria";//nome da procedimento BD bd_comercio
+                SqlCmd.CommandType = CommandType.StoredProcedure;//Executar tipo procedure
+
+                //Estanciar variaveis SQL (@id_categoria)
+                SqlParameter ParIdCategoria = new SqlParameter();
+                ParIdCategoria.ParameterName = "@id_categoria";
+                ParIdCategoria.SqlDbType = SqlDbType.Int;//Tratamento tipo
+                ParIdCategoria.Value = Categoria.Id_categoria;//Tratamento tipo
+                SqlCmd.Parameters.Add(ParIdCategoria);//Adicionar ao parâmetro
+
+                //Executar o comando = se exclusão informar: OK se não "Registro não foi Inserido!"
+                resp = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "A exclusão não foi realizada!";
+
+            }
+            catch (Exception ex)
+            {
+                //Mensagem de erro
+                resp = ex.Message;
+            }
+            finally
+            {
+                //Se a conexão for aberta e o status estiver aberta, então fechar conexão
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+        //Método Mostrar
+        public DataTable Mostrar(DCategoria Categoria) 
+        {
+            DataTable DtResultado = new DataTable("categoria");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Chamando o procedimento / procedure
+                SqlCon.ConnectionString = Conexao.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;//Receber a conexão
+                SqlCmd.CommandText = "spmostrar_categoria";//nome da procedimento BD bd_comercio
+                SqlCmd.CommandType = CommandType.StoredProcedure;//Executar tipo procedure
+                SqlDataAdapter sqlDat = new SqlDataAdapter(SqlCmd);//Mostrar usar SqlDataAdapter
+                sqlDat.Fill(DtResultado);//Preencher a tabela e exibir resultado
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;//Não mostrainformação se houver erro.
+            }
+        }
+        //Método Buscar Nome
+        public DataTable BuscarNome(DCategoria Categoria) 
+        {
+            DataTable DtResultado = new DataTable("categoria");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Chamando o procedimento / procedure
+                SqlCon.ConnectionString = Conexao.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;//Receber a conexão
+                SqlCmd.CommandText = "spbuscar_nome";//nome da procedimento BD bd_comercio
+                SqlCmd.CommandType = CommandType.StoredProcedure;//Executar tipo procedure
+                SqlDataAdapter sqlDat = new SqlDataAdapter(SqlCmd);//Mostrar usar SqlDataAdapter
+                sqlDat.Fill(DtResultado);//Preencher a tabela e exibir resultado
+
+                //Estanciar variaveis SQL (@descricao)
+                SqlParameter ParTextoBuscar = new SqlParameter();
+                ParTextoBuscar.ParameterName = "@testobuscar";
+                ParTextoBuscar.SqlDbType = SqlDbType.VarChar;
+                ParTextoBuscar.Size = 50;
+                ParTextoBuscar.Value = Categoria.TextoBuscar;
+                SqlCmd.Parameters.Add(ParTextoBuscar);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;//Não mostrainformação se houver erro.
+            }
+        }
     }
 }
---
+++ SysComercio/CamadaNegocio/NCategoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CamadaDados;
+using System.Data;
+
+namespace CamadaNegocio
+{
+    public class NCategoria
+    {
+        public static string Inserir(string nome, string descricao)
+        {
+            DCategoria Obj = new CamadaDados.DCategoria();
+            Obj.Nome = nome;
+            Obj.Descricao = descricao;
+            return Obj.Inserir(Obj);//Passar a DCategoria
+        }
+    }
+}        //Aula 31, 5min11seg
